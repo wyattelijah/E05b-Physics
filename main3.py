bbd363ee8fe3c5572edc054b6b005b29270b8c62
wyattelijah <wyatt.elijahd@gmail.com>
2019-09-30 19:06:04 -0400

    main3 done

--- main3.py
+++ main3.py
@@ -51,8 +51,12 @@ class Window(arcade.Window):
 
             collisions = a.collides_with_list(self.animal_list)
             for c in collisions:
-                # implement conservation of momentum here
-                # pass just does nothing. After you add your code, ou can delete what is now on line 56
+                tx = a.dx
+                ty = a.dy
+                a.dx = c.dx
+                a.dy = c.dy
+                c.dx = tx
+                c.dy = ty
                 pass
 
 
